895cbc308680ea8a7fa2ac14d1c150dafeab984e
poohitan <poohitan@gmail.com>
2017-12-22 13:03:08 +0200

    Fix flash of unstyled content

--- components/Wrapper.js
+++ components/Wrapper.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Head from 'next/head';
 import Header from './Header';
 import Content from './Content';
+import styles from '../styles/base.scss';
 
 const Wrapper = props => (
   <div className="wrapper">
+    <Head>
+      <style>{styles}</style>
+    </Head>
+
     <Header />
 
     <Content>
       {props.children}
     </Content>
+
+    <div className="wrapper-shadow" />
   </div>
 );
 
---
+++ pages/_document.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import Document, { Head, Main, NextScript } from 'next/document';
+import styles from '../styles/main.scss';
+
+export default class MyDocument extends Document {
+  static getInitialProps({ renderPage }) {
+    const {
+      html,
+      head,
+      errorHtml,
+      chunks,
+    } = renderPage();
+
+    return {
+      html,
+      head,
+      errorHtml,
+      chunks,
+    };
+  }
+
+  render() {
+    return (
+      <html lang="uk-UA">
+        <Head>
+          <title>poohitan</title>
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" key="viewport" />
+          <meta httpEquiv="content-type" content="text/html; charset=utf-8" />
+          <link rel="icon" href="/static/icons/favicon.ico" type="image/x-icon" />
+          <style>{styles}</style>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+}
