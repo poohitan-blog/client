9e0efbcb366d8cd93f76115a643d8cf8188885cb
poohitan <poohitan@gmail.com>
2017-12-23 01:50:43 +0200

    Make Header responsive

--- components/Header.js
+++ components/Header.js
@@ -4,18 +4,18 @@ import Trashbin from './header/Trashbin';
 
 export default () => (
   <nav className="header">
-    <ul className="menu layout-row layout-align-center">
+    <ul className="menu layout-gt-xs-row layout-align-gt-xs-center-start layout-column layout-align-start-center">
       <li>
-        <h1><Link href="/"><a>Головна</a></Link></h1>
+        <h1 className="menu-item"><Link href="/"><a>Головна</a></Link></h1>
       </li>
       <li>
-        <h1><Link href="/archive"><a>Архів</a></Link></h1>
+        <h1 className="menu-item"><Link href="/archive"><a>Архів</a></Link></h1>
       </li>
       <li>
-        <h1><Link as="/about" href="/page?path=about"><a>Про</a></Link></h1>
+        <h1 className="menu-item"><Link as="/about" href="/page?path=about"><a>Про</a></Link></h1>
       </li>
       <li>
-        <Link href="/trash"><a><Trashbin /></a></Link>
+        <div className="menu-item"><Link href="/trash"><a><Trashbin/></a></Link></div>
       </li>
     </ul>
   </nav>
