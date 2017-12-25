e14b79fb0166dec58ec0495a8112a7a778e0b085
poohitan <poohitan@gmail.com>
2017-12-25 23:47:35 +0200

    Implement data deserializer

---
+++ utils/deserialize.js
@@ -0,0 +1,30 @@
+const deserializers = {
+  [String]: value => value,
+  [Date]: value => new Date(value),
+  [Number]: value => Number(value),
+  [Boolean]: value => value === 'true',
+};
+
+const deserializeOne = (data, schema) => Object.keys(data).reduce((result, fieldName) => {
+  const fieldValue = data[fieldName];
+  const fieldType = schema[fieldName] || String;
+  let deserializedValue;
+
+  if (Array.isArray(fieldType)) {
+    const deserialize = deserializers[fieldType[0]];
+    deserializedValue = fieldValue.map(item => deserialize(item));
+  } else {
+    const deserialize = deserializers[fieldType];
+    deserializedValue = deserialize(fieldValue);
+  }
+
+  return Object.assign({ }, result, { [fieldName]: deserializedValue });
+}, {});
+
+export default (data, schema) => {
+  if (Array.isArray(data)) {
+    return data.map(object => deserializeOne(object, schema));
+  }
+
+  return deserializeOne(data, schema);
+};
