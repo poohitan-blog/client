5001c82b5fa476727b143b3394297cd7bd5e3c49
poohitan <poohitan@gmail.com>
2017-12-26 18:56:06 +0200

    Add tags to PostFooter component

--- components/post/Footer.js
+++ components/post/Footer.js
@@ -1,25 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Link from 'next/link';
 import * as Grammar from '../../services/grammar';
 import CommentIcon from '../../static/icons/comment.svg';
 import CalendarIcon from '../../static/icons/calendar.svg';
 
-const Footer = props => (
-  <div className="post-footer layout-row caption">
-    <div className="post-footer-comments layout-row layout-align-start-center">
-      <CommentIcon className="post-footer-icon post-footer-comments-icon" />
-      <span className="flex-offset-5 nowrap">
-        { Grammar.describeCommentsCount(props.commentsCount) }
-      </span>
+const Footer = (props) => {
+  const tags = props.tags
+    .map((tag) => {
+      const encodedTag = encodeURIComponent(tag);
+
+      return <Link key={tag} as={`/tag/${encodedTag}`} href={`/tag?tag=${encodedTag}`}><a>{tag}</a></Link>;
+    })
+    .reduce((previousTags, currentTag) => [previousTags, ', ', currentTag]);
+
+  return (
+    <div className="post-footer layout-row caption">
+      <div className="post-footer-comments layout-row layout-align-start-center">
+        <CommentIcon className="post-footer-icon post-footer-comments-icon" />
+        <span className="flex-offset-5 nowrap">
+          { Grammar.describeCommentsCount(props.commentsCount) }
+        </span>
+      </div>
+      <div className="post-footer-date layout-row layout-align-start-center flex-offset-5">
+        <CalendarIcon className="post-footer-icon post-footer-date-icon" />
+        <span className="flex-offset-5 nowrap">{ Grammar.formatPostDate(props.publishedAt) }</span>
+      </div>
+      <div className="post-footer-tags nowrap flex-offset-5">Теґи: { tags }</div>
+      <div className="post-footer-social" />
     </div>
-    <div className="post-footer-date layout-row layout-align-start-center flex-offset-5">
-      <CalendarIcon className="post-footer-icon post-footer-date-icon" />
-      <span className="flex-offset-5 nowrap">{ Grammar.formatPostDate(props.publishedAt) }</span>
-    </div>
-    <div className="post-footer-tags" />
-    <div className="post-footer-social" />
-  </div>
-);
+  );
+};
 
 Footer.propTypes = {
   commentsCount: PropTypes.number,
@@ -27,6 +38,7 @@ Footer.propTypes = {
     PropTypes.string,
     PropTypes.instanceOf(Date),
   ]).isRequired,
+  tags: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
 Footer.defaultProps = {
