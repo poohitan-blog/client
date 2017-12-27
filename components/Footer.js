476d3ae79fd04de303a56b60332baf0adbbfbf84
poohitan <poohitan@gmail.com>
2017-12-28 00:12:29 +0200

    Add different styles and weights of PT Sans font

--- components/Footer.js
+++ components/Footer.js
@@ -2,9 +2,9 @@ import React from 'react';
 import Searchbox from './Searchbox';
 
 const Footer = () => (
-  <div className="footer layout-row layout-align-space-between-center">
+  <div className="footer layout-row layout-wrap layout-align-space-between-center">
     <div />
-    <div className="flex-35 flex-xs-45"><Searchbox /></div>
+    <div className="flex-35 flex-xs-100"><Searchbox /></div>
   </div>
 );
 
