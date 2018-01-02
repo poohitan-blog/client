52f8c5f60a7c32bd5002cb2c575e3b94db9c231f
poohitan <poohitan@gmail.com>
2018-01-02 14:03:25 +0200

    Implement Checkbox UI component

---
+++ components/ui/Checkbox.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import CheckIcon from '../../static/icons/check.svg';
+
+class Checkbox extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.change = this.change.bind(this);
+  }
+
+  change(event) {
+    this.props.onChange(event.target.checked);
+  }
+
+  render() {
+    const { checked, label } = this.props;
+    const checkboxId = `checkbox-${label}`;
+
+    return (
+      <div className="checkbox">
+        <label htmlFor={checkboxId} className="layout-row layout-align-start-center">
+          <input id={checkboxId} type="checkbox" checked={checked} onChange={this.change} className="checkbox-native" />
+          <div className="checkbox-square">
+            {checked && <CheckIcon />}
+          </div>
+          <span className="checkbox-label">{label}</span>
+        </label>
+      </div>
+    );
+  }
+}
+
+Checkbox.propTypes = {
+  checked: PropTypes.bool,
+  label: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
+
+Checkbox.defaultProps = {
+  checked: false,
+};
+
+export default Checkbox;
