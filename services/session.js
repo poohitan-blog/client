c2aacc0a2043ef2ceab835a36a5ecbc291f7d6bf
poohitan <poohitan@gmail.com>
2018-01-05 19:32:28 +0200

    Decode JWT in browser to make sure token is valid

--- services/session.js
+++ services/session.js
@@ -1,4 +1,6 @@
 import Router from 'next/router';
+import decodeJWT from 'jwt-decode';
+
 import { current } from '../config';
 import Cookies from './cookies';
 import request from '../utils/request';
@@ -15,7 +17,14 @@ async function authenticate({ login, password }) {
 }
 
 function isAuthenticated(req) {
-  return Boolean(Cookies.get('token', req));
+  try {
+    const token = Cookies.get('token', req);
+    const decoded = decodeJWT(token);
+
+    return Boolean(decoded.id);
+  } catch (error) {
+    return false;
+  }
 }
 
 function getToken(req) {
