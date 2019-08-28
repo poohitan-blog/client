9de702d9c22fd6fb8d29b4927ba9bd5690349dc3
poohitan <poohitan@gmail.com>
2019-08-28 21:41:38 +0300

    Clean-up deploy task

--- tasks/deploy.js
+++ tasks/deploy.js
@@ -21,19 +21,13 @@ const privateKey = fs.readFileSync('/Users/poohitan/.ssh/id_rsa');
 
 const exec = command => execSSH({ host, username, privateKey })(`source ~/.profile && ${command}`);
 
-const envVariables = {
-  NODE_ENV: env,
-};
-
-const envVariablesString = Object.keys(envVariables).map(envVariableName => `export ${envVariableName}=${envVariables[envVariableName]}`).join(' && ');
-
 exec(`git clone -b ${branch} ${repo} ${folder}/new`)
   .then(() => exec(`npm install --prefix ${folder}/new`))
   .then(() => exec(`rm -rf ${folder}/current`))
   .then(() => exec(`mv ${folder}/new ${folder}/current`))
   .then(() => exec(`npm run build --prefix ${folder}/current`))
   .then(() => exec(`pm2 stop ${appName}`))
-  .then(() => exec(`${envVariablesString} && cd ${folder}/current && pm2 start server.js --name ${appName} --update-env`))
+  .then(() => exec(`export NODE_ENV=${env} && cd ${folder}/current && pm2 start server.js --name ${appName} --update-env`))
   .then(() => console.log('Deployed successfully.'))
   .catch(error => console.error(error))
   .then(() => process.exit());
