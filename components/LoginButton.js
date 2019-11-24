3395a4625e651bbc1eae9089fe6fc0bc058a708e
poohitan <poohitan@gmail.com>
2019-11-25 00:46:06 +0200

    Implement showing an angry emoji on login button click

--- components/LoginButton.js
+++ components/LoginButton.js
@@ -1,6 +1,52 @@
 import React from 'react';
 import Link from 'next/link';
+import random from '../helpers/random';
 
-export default () => (
-  <Link href="/login" as="/wardrobe"><div className="login-button" /></Link>
-);
+const angryEmojis = [
+  'o͡͡͡͡͡͡͡͡͡͡͡͡͡͡╮(;´༎ຶД༎ຶ`)╭o͡͡͡͡͡͡͡͡͡͡͡͡͡͡',
+  '╰(°ㅂ°)╯',
+  'ಠ▃ಠ',
+  '(; ⌣̀_⌣́)',
+  '(`･︿´･ )',
+  '୧༼ ಠ益ಠ ༽୨',
+];
+
+export default class LoginButton extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      alreadyClicked: false,
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+    const { alreadyClicked } = this.state;
+
+    if (alreadyClicked) {
+      return;
+    }
+
+    event.preventDefault();
+
+    const emojiIndex = random({ min: 0, max: angryEmojis.length });
+
+    global.alert(angryEmojis[emojiIndex]);
+
+    this.setState({
+      alreadyClicked: true,
+    });
+  }
+
+  render() {
+    return (
+      <Link href="/login" as="/wardrobe">
+        <a title="Не натискати" onClick={event => this.handleClick(event)}>
+          <div className="login-button" />
+        </a>
+      </Link>
+    );
+  }
+}
