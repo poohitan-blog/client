272ec424a85a4cfa2de73503c5a3583cf786f7a4
poohitan <poohitan@gmail.com>
2019-12-01 14:18:12 +0200

    Decrease chances to win the Raspberry lottery

--- services/raspberries.js
+++ services/raspberries.js
@@ -3,7 +3,7 @@ import Cookies from './cookies';
 import random from '../helpers/random';
 
 const COOKIE_NAME = 'raspberriesDay';
-const LUCKY_NUMBER = { min: 1, max: 7 };
+const LUCKY_NUMBER = { min: 1, max: 10 };
 
 function getCookie(req) {
   const cookie = Cookies.get(COOKIE_NAME, req);
